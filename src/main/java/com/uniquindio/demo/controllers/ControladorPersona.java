94ee159f28ab43352902a97234eee57017fe1c07
DanielaAndreaGonzalez <daniela.andrea.gonzalez@correounivalle.edu.co>
2023-03-29 09:40:24 -0500

    Se agrega el Swagger

--- src/main/java/com/uniquindio/demo/controllers/ControladorPersona.java
+++ src/main/java/com/uniquindio/demo/controllers/ControladorPersona.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.uniquindio.demo.interfazServices.IPersonaService;
 import com.uniquindio.demo.models.Persona;
@@ -23,7 +24,7 @@ import com.uniquindio.demo.models.Persona;
  *
  */
 
-@Controller
+@RestController
 @RequestMapping
 public class ControladorPersona {
 	
